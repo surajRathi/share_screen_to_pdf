7dde165c5a2c4492f1f4196999fd92d41ae6b7e7
Suraj Rathi <suraj.rathi00@gmail.com>
2021-04-23 15:57:42 +0530

    made it easy

--- main.js
+++ main.js
@@ -28,15 +28,14 @@ function remove(el) {
 
   let video = null; // Video of shared screen
   let auto_clicker = null; // Used with setInterval to automatically take pictures
-  let auto_click_interval = 500; // ms between auto screenshot
+  let auto_click_interval = 45 * 1000; // ms between auto screenshot
   // TODO: Expose status of auto clicker and auto_click_interval in HTML
 
-  let discard_similar_slides = true;
 
   let canvas = null; // The canvas element for capturing the image, it is hidden.
-  let prev_data = null; // ImageData Object,  previous image
 
   let output_div = null; // div which contains the taken screenshots
+  let auto_click_interval_input = null;
   let title = null; // Title element which can be edited by the user.
 
 
@@ -44,34 +43,6 @@ function remove(el) {
   //            - width
   //            - height
   //            - data : (r,g,b,a)_{x,y} = data[y * width + x: y * width + x + 4]
-
-  // Checks for similarity between images
-  function is_new_slide(img, prev_img) {
-    if (img === null) return false;
-    if (prev_img === null) return true;
-    if (img.width !== prev_img.width || img.height !== prev_img.height) return false;
-
-    // TODO: Implement
-
-    function diff_LSE() {
-      let err = 0;
-      //               R  G  B  A
-      const offsets = [0, 1, 2];
-      for (let i = 0; i < 4 * img.height * img.width; i += 4) {
-        offsets.forEach((offset) => err += (img.data[i + offset] - prev_img.data[i + offset]) ** 2);
-      }
-      err /= 3 * img.height * img.width;
-      $('diff_LSE').innerText = err;
-      return err;
-    }
-
-    // Use Squared Error
-    let threshold = 5000;
-    let err = diff_LSE();
-    return (err > threshold);
-
-    // return true;
-  }
 
   /*
    Capture a photo by fetching the current contents of the video
@@ -89,17 +60,14 @@ function remove(el) {
       context.drawImage(video, 0, 0, width, height);
       let cur_data = context.getImageData(0, 0, canvas.width, canvas.height);
 
-      if (check_similarity || !discard_similar_slides || is_new_slide(cur_data, prev_data)) {
-        // TODO: Use Blob?
-        let data = canvas.toDataURL('image/png');
-        let img = document.createElement('img');
-        img.className = "screenshot";
-        img.setAttribute('src', data);
-        img.setAttribute('onauxclick', 'remove(this)');
-        output_div.appendChild(img);
-        // output_div.appendChild(document.createElement('br'));
-        prev_data = cur_data;
-      }
+      // TODO: Use Blob?
+      let data = canvas.toDataURL('image/png');
+      let img = document.createElement('img');
+      img.className = "screenshot";
+      img.setAttribute('src', data);
+      img.setAttribute('onauxclick', 'remove(this)');
+      output_div.appendChild(img);
+
     }
   }
 
@@ -111,6 +79,8 @@ function remove(el) {
 
     video = $('video');
     $('click_button').onclick = () => take_screenshot(true);
+
+    $("auto_click_interval").innerText = auto_click_interval;
     $('auto_click').onclick = function () {
       if (auto_clicker === null) {
         auto_clicker = setInterval(take_screenshot, auto_click_interval);
@@ -124,9 +94,12 @@ function remove(el) {
       }
     }
 
-    $('discard_similar_slides').onclick = function () {
-      discard_similar_slides = !discard_similar_slides;
+    auto_click_interval_input = $('auto_click_interval_input')
+    auto_click_interval_input.onblur = function () {
+            auto_click_interval = parseInt(auto_click_interval_input.value);
+
     }
+    auto_click_interval_input.value = auto_click_interval;
 
     $('stop_button').onclick = function () {
       let tracks = video.srcObject.getTracks();
